147a3f3d42cbccce9e8d3f907a0b67f0661581ca
Mijj Herrera <guillenherrera.MFGH@gmail.com>
2025-08-15 06:02:58 +0000

    porque el header del cajero en la pagina de cierre de caja, tiene como 2

--- src/app/cashier/close-shift/page.tsx
+++ src/app/cashier/close-shift/page.tsx
@@ -1,6 +1,5 @@
 import { CloseShiftClientPage } from '@/components/cashier/CloseShiftClientPage';
 import { transactions } from '@/lib/data';
-import { AppHeader } from '@/components/AppHeader';
 
 export const dynamic = 'force-dynamic';
 
@@ -9,10 +8,7 @@ export default function CloseShiftPage() {
 
   return (
     <div className="min-h-screen bg-background">
-      <AppHeader title="Cierre de Caja" showBackButton={true} />
-      <main className="container mx-auto p-4 sm:p-6 lg:p-8">
-        <CloseShiftClientPage transactions={currentTransactions} />
-      </main>
+      <CloseShiftClientPage transactions={currentTransactions} />
     </div>
   );
 }
